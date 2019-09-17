b268f3f52c6b0440784b7562246df3fe1ce7d4a0
Stasencia <44728575+Stasencia@users.noreply.github.com>
2019-09-17 12:22:39 +0300

    Minor changes of how the code looks

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 var express = require('express');
-var path = require('path'); // модуль для парсинга пути
+var path = require('path'); // модуль для парсинга шляху
 var log = require('./libs/log')(module);
-var app = express(); app.use(express.favicon()); // отдаем стандартную фавиконку, можем здесь же свою задать
+var app = express(); app.use(express.favicon()); // задаємо стандартну іконку сайту, але можна також передати власну
 var config = require('./libs/config');
 var ArticleModel = require('./libs/mongoose').ArticleModel;
 
-app.use(express.logger('dev')); // выводим все запросы со статусами в консоль
-app.use(express.bodyParser()); // стандартный модуль, для парсинга JSON в запросах
-app.use(express.methodOverride()); // поддержка put и delete
-app.use(app.router); // модуль для простого задания обработчиков путей
-app.use(express.static(path.join(__dirname, "public"))); // запуск статического файлового сервера, который смотрит на папку public/ (в нашем случае отдает index.html)
+app.use(express.logger('dev')); // вивводимо всі запитизі статусами в консоль
+app.use(express.bodyParser()); // стандартный модуль для парсингу JSON в запитах
+app.use(express.methodOverride()); // підтримка put и delete
+app.use(app.router); // модуль для простого вказазння обробника шляхів
+app.use(express.static(path.join(__dirname, "public"))); // запуск статичного файлового серверу, що диивтсья на папку public/ (в нашому випадку віддає index.html)
 
-app.get('/api', function (req, res) {
+app.get('/api', function(req, res) {
     res.send('API is running');
 });
 
@@ -20,43 +20,43 @@ app.listen(config.get('port'), function(){
 });
 
 app.use(function(req, res, next){
-  res.status(404);
-  log.debug('Not found URL: %s',req.url);
-  res.send({ error: 'Not found' });
-  return;
+    res.status(404);
+    log.debug('Not found URL: %s', req.url);
+    res.send({ error: 'Not found' });
+    return;
 });
 
 app.use(function(err, req, res, next){
-  res.status(err.status || 500);
-  log.error('Internal error(%d): %s',res.statusCode,err.message); res.send({ error: err.message });
- 	return;
+    res.status(err.status || 500);
+    log.error('Internal error(%d): %s', res.statusCode, err.message); res.send({ error: err.message });
+ 	  return;
 });
 
 app.get('/ErrorExample', function(req, res, next){
-  next(new Error('Random error!'));
+    next(new Error('Random error!'));
 });
 
 app.get('/api/articles', function(req, res) {
-  return ArticleModel.find(function (err, articles) {
+    return ArticleModel.find(function (err, articles) {
         if (!err) {
             return res.send(articles);
         } else {
             res.statusCode = 500;
-            log.error('Internal error(%d): %s',res.statusCode,err.message);
+            log.error('Internal error(%d): %s', res.statusCode, err.message);
             return res.send({ error: 'Server error' });
         }
     });
 });
 
 app.post('/api/articles', function(req, res) {
-  var article = new ArticleModel({
+    var article = new ArticleModel({
         title: req.body.title,
         author: req.body.author,
         description: req.body.description,
         images: req.body.images
     });
 
-    article.save(function (err) {
+    article.save(function(err) {
         if (!err) {
             log.info("article created");
             return res.send({ status: 'OK', article:article });
@@ -69,13 +69,13 @@ app.post('/api/articles', function(req, res) {
                 res.statusCode = 500;
                 res.send({ error: 'Server error' });
             }
-            log.error('Internal error(%d): %s',res.statusCode,err.message);
+            log.error('Internal error(%d): %s', res.statusCode, err.message);
         }
     });
 });
 
 app.get('/api/articles/:id', function(req, res) {
-  return ArticleModel.findById(req.params.id, function (err, article) {
+    return ArticleModel.findById(req.params.id, function (err, article) {
         if(!article) {
             res.statusCode = 404;
             return res.send({ error: 'Not found' });
@@ -84,14 +84,14 @@ app.get('/api/articles/:id', function(req, res) {
             return res.send({ status: 'OK', article:article });
         } else {
             res.statusCode = 500;
-            log.error('Internal error(%d): %s',res.statusCode,err.message);
+            log.error('Internal error(%d): %s', res.statusCode, err.message);
             return res.send({ error: 'Server error' });
         }
     });
 });
 
-app.put('/api/articles/:id', function (req, res){
-  return ArticleModel.findById(req.params.id, function (err, article) {
+app.put('/api/articles/:id', function(req, res){
+    return ArticleModel.findById(req.params.id, function (err, article) {
         if(!article) {
             res.statusCode = 404;
             return res.send({ error: 'Not found' });
@@ -113,14 +113,14 @@ app.put('/api/articles/:id', function (req, res){
                     res.statusCode = 500;
                     res.send({ error: 'Server error' });
                 }
-                log.error('Internal error(%d): %s',res.statusCode,err.message);
+                log.error('Internal error(%d): %s', res.statusCode, err.message);
             }
         });
     });
 });
 
-app.delete('/api/articles/:id', function (req, res){
-  return ArticleModel.findById(req.params.id, function (err, article) {
+app.delete('/api/articles/:id', function(req, res){
+    return ArticleModel.findById(req.params.id, function(err, article) {
         if(!article) {
             res.statusCode = 404;
             return res.send({ error: 'Not found' });
@@ -131,7 +131,7 @@ app.delete('/api/articles/:id', function (req, res){
                 return res.send({ status: 'OK' });
             } else {
                 res.statusCode = 500;
-                log.error('Internal error(%d): %s',res.statusCode,err.message);
+                log.error('Internal error(%d): %s', res.statusCode, err.message);
                 return res.send({ error: 'Server error' });
             }
         });
